5998ff45945ea1b61294accafcf790ee1b7cd5c7
ByeongSu Do <bsdo@naver.com>
2018-06-26 23:48:24 +0900

    add graphics view

--- bschart_graphics.py
+++ bschart_graphics.py
@@ -1,50 +1,238 @@
-from PyQt5 import QtGui, QtCore
-from PyQt5.QtCore import QTranslator, QObject
-from PyQt5.QtGui import QColor, QPainterPath
-from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem, QStyleOptionGraphicsItem
+import time
+
+import numpy as np
+import pandas as pd
+
+from PyQt5 import QtGui
+from PyQt5.QtCore import QObject, Qt, QRectF
+from PyQt5.QtGui import QColor, QPainterPath, QPen
+from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem, QStyleOptionGraphicsItem, \
+    QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QGraphicsSceneMouseEvent, QGraphicsSceneHoverEvent, \
+    QGraphicsSceneWheelEvent
+from scipy.linalg import norm
 
 
 class AxisItem(QGraphicsItem):
-    def __init__(self, scene, parent=None):
+    def __init__(self, data: pd.DataFrame, parent=None):
         QGraphicsItem.__init__(self, parent)
-        self.moveBy(-scene.width() / 2, -scene.height() / 2)
+        # self.setAcceptHoverEvents(True)
+        self.data = data
+        self.max = data['close'].max()
+        self.min = data['close'].min()
+        self.length = data['close'].size
+        self.setZValue(1)
 
     def paint(self, painter: QtGui.QPainter, option: 'QStyleOptionGraphicsItem', widget=None):
+        view = self.scene().views()[0]
+        height = view.height()
+        width = view.width()
+
+        count = float(width / self.length)
+
+        s = time.perf_counter()
+        painter.save()
         path = QPainterPath()
-        path.moveTo(-4, 0)
-        path.lineTo(-4, 233)
-        path.moveTo(93, 0)
-        path.lineTo(93, 233)
-        path.moveTo(190, 0)
-        path.lineTo(190, 233)
-        path.moveTo(287, 0)
-        path.lineTo(287, 233)
-        path.moveTo(384, 0)
-        path.lineTo(384, 233)
+        for i in np.arange(self.length):
+            path.moveTo(count * i, 0)
+            path.lineTo(count * i, height)
 
         painter.setPen(QColor('#363c4e'))
         painter.drawPath(path)
+        painter.restore()
+        print("Timer:AxisItem() : ", (time.perf_counter() - s)*1000)
+
+    def boundingRect(self):
+        view = self.scene().views()[0]
+        rect = view.rect()
+
+        return QRectF(int(rect.x()), int(rect.y()),
+                      int(rect.width()), int(rect.height()))
 
 
-class GraphWidget(QGraphicsView):
+class PlotItem(QGraphicsItem):
+    def __init__(self, data: pd.DataFrame, parent=None):
+        QGraphicsItem.__init__(self, parent)
+        self.setAcceptHoverEvents(True)
+        self.data = data
+        self.max = data['close'].max()
+        self.min = data['close'].min()
+        self.length = data['close'].size
+        self.mouse_over = False
+        self.cached = False
+        self.path = None
+        self.delta = 0
+        self.setZValue(2)
+
+    def paint(self, painter: QtGui.QPainter, option: 'QStyleOptionGraphicsItem', widget=None):
+        flip = -1
+        view = self.scene().views()[0]
+        height = view.height()
+        width = view.width()
+
+        margin_max = self.max + 10
+        margin_min = self.min - 10
+        gap = margin_max - margin_min
+        count = float(width / self.length)
+        count_v = float(height / gap)
+
+        s = time.perf_counter()
+        painter.save()
+        self.data['x'] = np.arange(self.length) * count + self.delta
+        self.data['y'] = (self.data['close'] - margin_min) * count_v * flip + height
+
+        # path = self.path
+        # if path is None and self.cached is False:
+        #     path = QPainterPath()
+        #     for i in np.arange(self.length - 1):
+        #         path.moveTo(self.data['x'][i], self.data['y'][i])
+        #         path.lineTo(self.data['x'][i+1], self.data['y'][i+1])
+        #
+        #     self.path = path
+        #     self.cached = True
+
+        path = QPainterPath()
+        for i in np.arange(self.length - 1):
+            path.moveTo(self.data['x'][i], self.data['y'][i])
+            path.lineTo(self.data['x'][i+1], self.data['y'][i+1])
+
+        print("Timer:PlotItem() : ", (time.perf_counter() - s) * 1000)
+
+        pen = QPen(Qt.white)
+        if self.mouse_over:
+            pen.setWidth(2)
+            view.setCursor(Qt.PointingHandCursor)
+        else:
+            view.setCursor(Qt.ArrowCursor)
+
+        painter.setPen(pen)
+        painter.drawPath(path)
+
+        painter.restore()
+
+    def boundingRect(self):
+        view = self.scene().views()[0]
+
+        rect = view.rect()
+        return QRectF(int(rect.x()), int(rect.y()),
+                      int(rect.width()), int(rect.height()))
+
+    def hoverMoveEvent(self, event: 'QGraphicsSceneHoverEvent'):
+        self.mouse_over = False
+        pos = self.mapToScene(event.pos())
+
+        print()
+        print("Scene Pos : ", pos)
+
+        data = self.data
+        dist = (pos.x() - data['x']) ** 2 + (pos.y() - data['y']) ** 2
+
+        min_pos_idx = dist.idxmin()
+        self.update()
+        if 1 < min_pos_idx < dist.size - 1:
+            min_pos = (data['x'][min_pos_idx], data['y'][min_pos_idx])
+            min_pos_minus = (data['x'][min_pos_idx + 1], data['y'][min_pos_idx + 1])
+            min_pos_plus = (data['x'][min_pos_idx - 1], data['y'][min_pos_idx - 1])
+
+            a2 = (min_pos[0] - min_pos_minus[0], min_pos[1] - min_pos_minus[1])
+            a3 = (pos.x() - min_pos_minus[0], pos.y() - min_pos_minus[1])
+
+            b2 = (min_pos[0] - min_pos_plus[0], min_pos[1] - min_pos_plus[1])
+            b3 = (pos.x() - min_pos_plus[0], pos.y() - min_pos_plus[1])
+
+            print("dot : ", np.dot(a3, b3))
+            if np.dot(a3, b3) < 1000:
+
+                # 2D distance = abs(np.cross(p2 - p1, p3 - p1) / norm(p2 - p1))
+                min_dist1 = abs(np.cross(a2, a3) / norm(a2))
+                min_dist2 = abs(np.cross(b2, b3) / norm(b2))
+
+                min_dist = min(min_dist1, min_dist2)
+                print("min_dist : ", min_dist)
+                if min_dist < 2:
+                    self.mouse_over = True
+                    self.update()
+
+        super().hoverMoveEvent(event)
+
+    def wheelEvent(self, event: 'QGraphicsSceneWheelEvent'):
+
+        self.delta += event.delta()
+        self.update()
+
+        super().wheelEvent(event)
+
+
+class GraphScene(QGraphicsScene):
+    def __init__(self, parent=None):
+        QGraphicsScene.__init__(self, parent)
+
+    def mouseMoveEvent(self, event: 'QGraphicsSceneMouseEvent'):
+
+        super().mouseMoveEvent(event)
+
+
+class GraphView(QGraphicsView):
     def __init__(self, parent=None):
         QGraphicsView.__init__(self, parent)
 
-        scene = QGraphicsScene(self)
-        scene.setSceneRect(-200, -200, 400, 400)
-        self.setScene(scene)
+        self.setMouseTracking(True)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setBackgroundBrush(QColor('#131722'))
         self.setMinimumSize(400, 400)
         self.setWindowTitle(QObject.tr(self, 'Graph'))
+        self.setFrameStyle(self.NoFrame)
 
-        self.setBackgroundBrush(QColor('#131722'))
+    def resizeEvent(self, event: QtGui.QResizeEvent):
+        print()
+        print("View Rect : ", self.rect())
+        self.setSceneRect(self.rect().x(), self.rect().y(),
+                          self.rect().width(), self.rect().height())
+        print("Scene Rect : ", self.sceneRect())
 
-        axis = AxisItem(scene)
-        scene.addItem(axis)
+        super().resizeEvent(event)
+
+    def mouseMoveEvent(self, event: QtGui.QMouseEvent):
+
+        super().mouseMoveEvent(event)
 
 
 if __name__ == '__main__':
     app = QApplication([])
 
-    win = GraphWidget()
+    data = pd.read_pickle('bitmex_1m_2018.pkl')
+    data = data[-2160:].reset_index()
+
+    plot_data = data
+
+    win = QWidget()
+    hlayout = QHBoxLayout()
+
+    view = GraphView()
+    scene = GraphScene()
+    view.setScene(scene)
+    axis = AxisItem(data)
+    plot = PlotItem(data)
+    view.scene().addItem(axis)
+    view.scene().addItem(plot)
+
+    button1 = QPushButton('move left')
+    button2 = QPushButton('move right')
+    button3 = QPushButton('zoom in')
+    button4 = QPushButton('zoom out')
+
+    vlayout = QVBoxLayout()
+    group = QWidget()
+    group.setLayout(vlayout)
+    vlayout.addWidget(button1)
+    vlayout.addWidget(button2)
+    vlayout.addWidget(button3)
+    vlayout.addWidget(button4)
+
+    hlayout.addWidget(view)
+    hlayout.addWidget(group)
+
+    win.setLayout(hlayout)
     win.show()
+
     app.exec_()
---
+++ request_bitmex_data.py
@@ -0,0 +1,110 @@
+import datetime
+
+import dateutil
+from PyQt5.QtCore import QTimer, Qt, QThread, QCoreApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from lib.source_clients.bitmexhttpclient import BitmexHttpClient
+from lib.source_clients.auth import bitmex
+
+import pandas as pd
+
+
+class Request:
+    def __init__(self, start=0, data=pd.DataFrame()):
+        self.client = BitmexHttpClient(test=False,
+                                       api_key=bitmex.api_keys['real']['order']['key'],
+                                       api_secret=bitmex.api_keys['real']['order']['secret'])
+
+        self.now = datetime.datetime.now(tz=datetime.timezone.utc)
+        self.one_year_min = 525600
+        self.rate_limit = 300
+        self.df = data
+        self.requested = 0
+        self.count = 500
+        # self.start = 0  # 2017 - 01- 01
+        self.start = start
+
+        self.year_first = datetime.datetime(self.now.year, 1, 1, tzinfo=datetime.timezone.utc)
+        self.now_to_min = (self.now - self.year_first).total_seconds() / 60
+        self.now_from_start = (self.now - (self.year_first + datetime.timedelta(minutes=self.start))).total_seconds() / 60
+
+        self.client.sig_ended.connect(self.get_data)
+
+    def request_data(self):
+        start = self.start
+        n = self.requested
+        s = self.count
+
+        self.client.Trade.get_bucketed('1m', symbol='XBTUSD', count=500, start=n * s + start)
+        self.requested += 1
+
+    def get_data(self):
+        start = self.start
+        n = self.requested
+        s = self.count
+        j = self.client.json()
+        self.rate_limit = self.client.headers()['x-ratelimit-remaining']
+
+        if len(j) > 0:
+            self.df = self.df.append(j, ignore_index=True)
+        elif len(j) == 0:
+            self.df.to_pickle('bitmex_1m_2018.pkl')
+            print('saved !')
+            print("Last index : {}".format(self.df.shape[0]))
+            QCoreApplication.quit()
+
+        print('now: {}, current: {}, percent: {:.2f}%, rate_limit: {}'
+              .format(self.now_to_min + self.one_year_min * (self.now.year - 2017), n * s + start,
+                      n * s / self.now_from_start * 100, self.rate_limit))
+
+        if s * n >= 525600 * 2:
+            self.df.to_pickle('bitmex_1m_2018_end.pkl')
+            print('saved to the end of {}!'.format(self.now.year))
+            QCoreApplication.quit()
+
+
+class Requester:
+    def __init__(self):
+
+        start, data = self.check_current_data()
+
+        self.timer = QTimer()
+        self.thread = QThread()
+        self.r = Request(start=start, data=data)
+
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.r.request_data)
+
+    def exec(self):
+        self.timer.start()
+        self.timer.moveToThread(self.thread)
+        self.thread.start()
+
+    def check_current_data(self):
+        now = datetime.datetime.now()
+        total_min = 525600 # 1 - year
+
+        df = pd.read_pickle('bitmex_1m_{}.pkl'.format(now.year))
+        last_time = df['timestamp'][df.shape[0] - 1]
+        expect_last_time = datetime.datetime(now.year, 1, 1, tzinfo=datetime.timezone.utc) + \
+                     datetime.timedelta(minutes=df.shape[0] - 1)
+
+        if expect_last_time == dateutil.parser.parse(last_time):
+            start_from = df.shape[0] + total_min * (now.year - 2017)
+            print("Now : ", now)
+            print("Last time : ", last_time)
+            print("Start from ...{}".format(start_from))
+            print()
+        else:
+            raise Exception("Not correct expected last time")
+
+        return start_from, df
+
+
+if __name__ == '__main__':
+    app = QCoreApplication([])
+
+    handler = Requester()
+    handler.exec()
+
+    app.exec()

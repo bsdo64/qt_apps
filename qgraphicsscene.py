33ddc0d5776f6fe3d346c5a65b3489a9c6a83338
ByeongSu Do <bsdo@naver.com>
2018-07-02 16:15:23 +0900

    add collide rect

--- qgraphicsscene.py
+++ qgraphicsscene.py
@@ -2,7 +2,7 @@ import pandas
 
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt, QRectF, QRect
-from PyQt5.QtGui import QRadialGradient, QGradient, QPen, QPainterPath, QTransform, QPainter
+from PyQt5.QtGui import QRadialGradient, QGradient, QPen, QPainterPath, QTransform, QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsSceneMouseEvent, QGraphicsItem, \
     QStyleOptionGraphicsItem, QWidget, QGraphicsRectItem
 
@@ -13,6 +13,9 @@ class MyItem(QGraphicsItem):
         self.num = 1
         self.data = data
         self.path = QPainterPath()
+        self.trans = QTransform()
+        self.cached = False
+        self.printed = False
         self.setZValue(0)
 
     def paint(self, painter: QtGui.QPainter, option: 'QStyleOptionGraphicsItem', widget: QWidget = ...):
@@ -21,18 +24,26 @@ class MyItem(QGraphicsItem):
         close_max = close.max()
         close_min = close.min()
 
-        for i in range(close.size - 1):
-            self.path.moveTo(i, close[i])
-            self.path.lineTo(i+1, close[i+1])
+        if not self.cached:
+            for i in range(close.size - 1):
+                self.path.moveTo(i, close[i])
+                self.path.lineTo(i+1, close[i+1])
 
-        painter.setPen(QPen(Qt.white))
+            self.cached = True
+
+        pen = QPen(Qt.white)
+        pen.setCosmetic(True)
+        painter.setPen(pen)
         painter.drawRect(0, 0, close.size, close_max - close_min)
 
-        painter.setPen(QPen(Qt.yellow))
+        pen.setColor(Qt.yellow)
+        painter.setPen(pen)
         painter.drawPath(self.path)
 
-        painter.setPen(QPen(Qt.red))
-        painter.drawEllipse(self.boundingRect())
+        if not self.printed:
+            rec_item = self.scene().addPath(self.path, QPen(Qt.red))
+            rec_item.setZValue(-10)
+            self.printed = True
 
     def boundingRect(self):
         close = self.data['close'].reset_index()['close']
@@ -47,26 +58,28 @@ class MyScene(QGraphicsScene):
         QGraphicsScene.__init__(self, parent)
         self.data = data
         self.rec = QGraphicsRectItem()
+        self.plot: MyItem(data) = None
         self.bounding_rect = QGraphicsRectItem()
-        self.setBackgroundBrush(Qt.blue)
+        self.setBackgroundBrush(QColor('#14161f'))
 
         self.addItem(self.bounding_rect)
+        self.printed = False
 
     def mouseMoveEvent(self, event: 'QGraphicsSceneMouseEvent'):
         print()
-        print("rec pos : ", self.rec.pos())
+
         print("rec rect : ", self.rec.rect())
         print("Scene rect : ", self.sceneRect())
         print("ItemBounding rect : ", self.itemsBoundingRect())
-        item = self.itemAt(event.scenePos(), QTransform())
+        print("transform : ", self.plot.transform().m11(), ", ", self.plot.transform().m22())
+        item = self.itemAt(event.scenePos(), self.plot.transform())
 
         if item and isinstance(item, MyItem):
-            print()Z
-            print('haha')
-            print(self.rec.pos())
-            print(self.rec.scenePos())
-            print(self.rec.collidesWithPath(item.path))
-            print(item.contains(event.scenePos()))
+            print()
+            print('collides path : ', self.rec.collidesWithPath(item.path))
+            print('collides item : ', self.rec.collidesWithItem(item))
+
+
 
         super().mouseMoveEvent(event)
 
@@ -88,34 +101,49 @@ class MyView(QGraphicsView):
         print("pixel / Data : {}".format(self.width() / len(self.data)))
 
     def resizeEvent(self, event: QtGui.QResizeEvent):
-        self.scene().setSceneRect(self.rect().x(), self.rect().y(),
+        scene: MyScene = self.scene()
+        scene.setSceneRect(self.rect().x(), self.rect().y(),
                           self.rect().width(), self.rect().height())
 
-        self.scene().print_bound(self.rect())
+        scene.print_bound(self.rect())
+
+        plot: QGraphicsItem = scene.plot
+        trans = QTransform()
+        close = plot.data['close'].reset_index()['close']
+        close = close - close.min()
+        close_max = close.max()
+        close_min = close.min()
+        trans.scale(self.width() / len(close),
+                    self.height() / (close_max - close_min))
+        plot.trans = trans
+        plot.setTransform(trans)
+
 
     def mouseMoveEvent(self, event: QtGui.QMouseEvent):
         rec: QGraphicsRectItem = self.scene().rec
-        rec.setPos(self.mapToScene(event.pos()))
+        rec.setRect(event.pos().x() - 5, event.pos().y() - 5, 10, 10)
 
         super().mouseMoveEvent(event)
 
 
 if __name__ == '__main__':
     df = pandas.read_pickle('bitmex_1m_2018.pkl')
-    data = df[-100:]
+    data = df[-5000:]
 
     app = QApplication([])
     scene = MyScene(data)
     view = MyView(data)
     view.setScene(scene)
-    view.show()
 
-    scene.addLine(0, 0, 10, 10, QPen(Qt.white))
     rec = QGraphicsRectItem(-2, -2, 4, 4)
-    rec.setPen(Qt.red)
+    rec.setPen(Qt.white)
     scene.rec = rec
     scene.addItem(rec)
 
-    scene.addItem(MyItem(data))
+    plot = MyItem(data)
+    scene.addItem(plot)
+    scene.plot = plot
+
+    view.show()
 
     app.exec_()
